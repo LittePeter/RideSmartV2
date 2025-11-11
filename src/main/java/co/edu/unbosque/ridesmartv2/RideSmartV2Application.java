93405c5c6483a8fd13ff6eb4f036f1e379f74e1b
Moisés Guerra <moa.guerra99@gmail.com>
2025-11-11 00:11:40 -0500

    Init/ First commit. All dependencies added

---
+++ src/main/java/co/edu/unbosque/ridesmartv2/RideSmartV2Application.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.ridesmartv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideSmartV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RideSmartV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/unbosque/ridesmartv2/RideSmartV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.ridesmartv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideSmartV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
