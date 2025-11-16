747f2f58bbd2cbc093e26467a050efdce32958d4
Moisés Guerra <moa.guerra99@gmail.com>
2025-11-15 19:57:44 -0500

    Documentación Modulo de Email

--- src/main/java/co/edu/unbosque/ridesmartv2/emailModule/service/EmailService.java
+++ src/main/java/co/edu/unbosque/ridesmartv2/emailModule/service/EmailService.java
@@ -1,5 +1,14 @@
 package co.edu.unbosque.ridesmartv2.emailModule.service;
-
+/**
+ * Define el contrato para el envío de correos electrónicos en el sistema RideSmart.
+ */
 public interface EmailService {
+    /**
+     * Envía un correo electrónico al destinatario especificado.
+     *
+     * @param to el correo electrónico del destinatario.
+     * @param subject el asunto del mensaje.
+     * @param body el cuerpo del mensaje.
+     */
     void sendMail(String to, String subject, String body);
 }
